cc95cb094871f2166bd826c170ca74ab699fdb0d
niuyunsi <niuyunsi@gmail.com>
2018-12-11 11:58:14 -0800

    remove package-lock.json

--- src/background.js
+++ src/background.js
@@ -19,8 +19,6 @@ function createWindow() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  //   mainWindow.loadFile('index.html');
-  console.log('startUrl', startUrl);
   mainWindow.loadURL(startUrl);
 
   // Open the DevTools.
