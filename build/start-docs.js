d3e9fc38c6b5ee936ad7f66ad2e88dd3b89dcfc1
Yunsi Niu <niuyunsi@gmail.com>
2018-12-30 21:15:55 -0800

    add docs script

---
+++ build/start-docs.js
@@ -0,0 +1,21 @@
+const childProcess = require('child_process');
+const webpack = require('webpack');
+const config = require('./webpack.docs.config');
+
+const env = 'development';
+const compiler = webpack(config(env));
+
+let serverStarted = false;
+
+const watching = compiler.watch({}, (err, stats) => {
+  console.log('compiling...done');
+  if (!err && !stats.hasErrors() && !serverStarted) {
+    serverStarted = true;
+
+    childProcess
+      .spawn('http-server', ['docs/', '-p 7777'])
+      .on('close', () => {
+        watching.close();
+      });
+  }
+});
